b5d586ecb60b7543928676bfb8a4afe0226ecb96
infiniteloop <aksagg@iitk.ac.in>
2016-01-10 21:37:05 +0530

    Calculator skeleton

--- web-server/routes.js
+++ web-server/routes.js
@@ -12,6 +12,49 @@ router.get('/api', function(req, res, next) {
 		res.status(400).json({'error':'invalid format'});
 		return;
 	}
+	try 
+	{	
+		a = parseFloat(req.query.a);
+		b = parseFloat(req.query.b);
+		c = parseInt(req.query.op);
+
+	} catch (ex) {
+		res.status(400).json({'error':'Invalid numbers'});
+		return;
+	}
+	if (!a || !b || isNaN(a) || isNaN(b))
+	{
+		 res.status(400).json({'error':'Invalid numbers'});
+		 return;
+	}
+	if (!c || c<0 || c>4)
+	{
+		res.status(400).json({'error':'Invalid operator'});
+		 return;
+	}
+	//Check cache
+
+
+	//If not found then compute
+	if (c==1)
+	{
+
+	}
+	else if(c==2)
+	{
+
+	}
+	else if(c==3)
+	{
+
+	}
+	else if(c==4)
+	{
+
+	}
+	console.log(a);
+	console.log(b);
+	console.log(c);
 	res.json({ 'result': 'OK' });
 });
 
