03d2d78bee00b9a0539dc7d5bf30175e2bb15b3c
infiniteloop <aksagg@iitk.ac.in>
2016-01-13 23:58:17 +0530

    Added sample network call

--- AkshayDAC/app/src/main/java/com/nonexistent/akshayaggarwal/akshaydac/MainActivity.java
+++ AkshayDAC/app/src/main/java/com/nonexistent/akshayaggarwal/akshaydac/MainActivity.java
@@ -6,7 +6,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -44,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
         input = (TextView) findViewById(R.id.text_input);
         result = (TextView) findViewById(R.id.text_result);
         state = -1;
+        queue = Volley.newRequestQueue(this);
         setState(0);
     }
 
@@ -81,6 +87,24 @@ public class MainActivity extends AppCompatActivity {
 
     public void press3(View view) {
 
+        String url = getResources().getString(R.string.web_server);
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        result.setText("Response is: "+ response.substring(0,10));
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        result.setText("That didn't work!");
+                    }
+                }
+        );
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
         setState(0);
     }
 
